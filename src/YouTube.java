24ad08cdff59c73b0466e4bc35f86b1862be6608
montanabraswell <montanabraswell1@gmail.com>
2016-12-27 22:57:33 -0500

    First Commit

---
+++ src/YouTube.java
@@ -0,0 +1,27 @@
+/**Java Tutorial 1
+ * establishing a class and method names
+ * also using a print statement
+ * @author Montana
+ */
+
+// everything begins in a class
+
+public class YouTube {
+	//put method here
+	//method is a recipe on how you wanna run your program
+
+	//method header(substitute for method and name)
+	//bakecake is name of the method
+	// void bakecake()
+	
+	
+	// also method header
+	public static void main(String args[]){
+		//method body
+		System.out.println("Hello YouTube");
+		
+	}
+
+
+
+}	
